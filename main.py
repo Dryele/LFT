6bfbdb99f1b7330b159adc1ed3fadbf601164e3b
Dryele <38793915+Dryele@users.noreply.github.com>
2021-09-22 14:49:25 +0000

    atualizando as palavras reservadas

--- main.py
+++ main.py
@@ -1,31 +1,35 @@
 import ply.lex as lex
 
-tokens = ['SELECAO', 'INSERIR','INTO', 'ATUALIZAR','APAGAR', 'WHERE', 'ORDER','BY', 'GROUP', 'HAVING','FROM']
+# colocar todas as palavras reservadas da linguagem aqui! 
+reserved = {'SELECT':'SELECT', 'INSERT':'INSERT','INTO':'INTO', 'UPDATE':'UPDATE','DELETE':'DELETE', 'WHERE':'WHERE','ORDER':'ORDER','BY':'BY', 'GROUP':'GROUP', 'HAVING':'HAVING','FROM':'FROM', 'VALUES':'VALUES', 'SET':'SET', 'CREATE':'CREATE', 'TABLE':'TABLE',  'ALTER':'ALTER', 'DECLARE':'DECLARE', 'PROCEDURE':'PROCEDURE', 'AS':'AS', 'OR':'OR', 'BEGIN':'BEGIN', 'IF':'IF', 'SET':'SET', 'END':'END', 'OUTPUT':'OUTPUT', 'PRINT':'PRINT','EXEC':'EXEC', 'ISNULL':'ISNULL', 'FOR':'FOR', 'LEFT':'LEFT', 'JOIN':'JOIN', 'ON':'ON','OPEN':'OPEN', 'FETCH':'FETCH','WHILE':'WHILE','ELSE':'ELSE', 'DEALLOCATE':'DEALLOCATE', 'DO':'DO', 'CLOSE':'CLOSE', 'NOT':'NOT', 'NULL':'NULL', 'INNER':'INNER','RIGHT':'RIGHT','DISTINCT':'DISTINCT','DATEPART':'DATEPART','ISNULL':'ISNULL'}
+tokens = ['ID', 'NUMERO', 'MUL', 'DIV']+list(reserved.values())
 t_ignore = ' \t'
-t_SELECAO = r'SELECT'
-t_INSERIR = r'INSERT'
-t_ATUALIZAR = r'UPDATE'
-t_APAGAR = r'DELETE'
+
+#reconhcer identificador, inicio de nomes e simbolos da linguagem
+def t_NUMERO(t):
+  r'[1-9][0-9]*(\.[0-9]+)?([eE][0-9]+)?'
+  return t
+
+def t_ID(t):
+  r'[a-zA-Z@_0-9][a-zA-Z_0-9]*(\.[a-zA-Z_0-9]+)?'
+  t.type = reserved.get(t.value.upper(), 'ID')
+  return t
+
+def t_COMENTARIO(t):
+  r'--'
+  pass
+
+t_MUL = r'\*'
+t_DIV =r'/'
+
+
 
 
 analisador = lex.lex()
-teste = "SELECT"
-teste2 = "INSERT"
-teste3 = "UPDATE"
-teste4 = "DELETE"
+teste = "Select VALOR TABLE.LINHA SET 25 * /"
 
 analisador.input(teste)
 for token in analisador:
   print(token.type, token.value)
 
-analisador.input(teste2)
-for token in analisador:
-  print(token.type, token.value)
 
-analisador.input(teste3)
-for token in analisador:
-  print(token.type, token.value)
-
-analisador.input(teste4)
-for token in analisador:
-  print(token.type, token.value)
