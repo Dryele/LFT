b011e2be2f08294be1b0fe1c21cd2c3a2a9d8f82
Dryele <38793915+Dryele@users.noreply.github.com>
2021-09-22 17:01:55 +0000

    OPERADORES

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import ply.lex as lex
 
 # colocar todas as palavras reservadas da linguagem aqui! 
 reserved = {'SELECT':'SELECT', 'INSERT':'INSERT','INTO':'INTO', 'UPDATE':'UPDATE','DELETE':'DELETE', 'WHERE':'WHERE','ORDER':'ORDER','BY':'BY', 'GROUP':'GROUP', 'HAVING':'HAVING','FROM':'FROM', 'VALUES':'VALUES', 'SET':'SET', 'CREATE':'CREATE', 'TABLE':'TABLE',  'ALTER':'ALTER', 'DECLARE':'DECLARE', 'PROCEDURE':'PROCEDURE', 'AS':'AS', 'OR':'OR', 'BEGIN':'BEGIN', 'IF':'IF', 'SET':'SET', 'END':'END', 'OUTPUT':'OUTPUT', 'PRINT':'PRINT','EXEC':'EXEC', 'ISNULL':'ISNULL', 'FOR':'FOR', 'LEFT':'LEFT', 'JOIN':'JOIN', 'ON':'ON','OPEN':'OPEN', 'FETCH':'FETCH','WHILE':'WHILE','ELSE':'ELSE', 'DEALLOCATE':'DEALLOCATE', 'DO':'DO', 'CLOSE':'CLOSE', 'NOT':'NOT', 'NULL':'NULL', 'INNER':'INNER','RIGHT':'RIGHT','DISTINCT':'DISTINCT','DATEPART':'DATEPART','ISNULL':'ISNULL'}
-tokens = ['ID', 'NUMERO', 'MUL', 'DIV']+list(reserved.values())
+tokens = ['ID', 'NUMERO', 'MUL', 'DIV', 'SOMA', 'SUB', 'PERCE']+list(reserved.values())
 t_ignore = ' \t'
 
 #reconhcer identificador, inicio de nomes e simbolos da linguagem
@@ -21,12 +21,15 @@ def t_COMENTARIO(t):
 
 t_MUL = r'\*'
 t_DIV =r'/'
+t_SUB = r'-'
+t_SOMA =r'\+'
+t_PERCE =r'%'
 
 
 
 
 analisador = lex.lex()
-teste = "Select VALOR TABLE.LINHA SET 25 * /"
+teste = "Select VALOR TABLE.LINHA SET 25 * / - + %"
 
 analisador.input(teste)
 for token in analisador:
